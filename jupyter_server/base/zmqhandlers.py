e78dd1dc3a61187e1a2679e8bc715eaae8222089
Steven Silvester <steven.silvester@ieee.org>
2022-03-12 06:25:02 -0600

    avoid usage of ipython_genutils

--- jupyter_server/base/zmqhandlers.py
+++ jupyter_server/base/zmqhandlers.py
@@ -9,7 +9,6 @@ import sys
 from urllib.parse import urlparse
 
 import tornado
-from ipython_genutils.py3compat import cast_unicode
 
 try:
     from jupyter_client.jsonutil import json_default
@@ -236,8 +235,7 @@ class ZMQStreamHandler(WebSocketMixin, WebSocketHandler):
             buf = serialize_binary_message(msg)
             return buf
         else:
-            smsg = json.dumps(msg, default=json_default)
-            return cast_unicode(smsg)
+            return json.dumps(msg, default=json_default)
 
     def _on_zmq_reply(self, stream, msg_list):
         # Sometimes this gets triggered when the on_close method is scheduled in the
@@ -275,7 +273,7 @@ class AuthenticatedZMQStreamHandler(ZMQStreamHandler, JupyterHandler):
             raise web.HTTPError(403)
 
         if self.get_argument("session_id", False):
-            self.session.session = cast_unicode(self.get_argument("session_id"))
+            self.session.session = self.get_argument("session_id")
         else:
             self.log.warning("No session ID specified")
 
