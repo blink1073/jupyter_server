b9564236d750b01e1f1461e9333d9828ba6081ce
FriendlyBot <meeseeksmachine@gmail.com>
2022-03-22 15:20:10 +0000

    Apply pre-commit

--- RELEASE.md
+++ RELEASE.md
@@ -2,9 +2,6 @@
 
 ## Using `jupyter_releaser`
 
-
-
-
 The recommended way to make a release is to use [`jupyter_releaser`](https://github.com/jupyter-server/jupyter_releaser#checklist-for-adoption).
 
 ## Manual Release
--- jupyter_server/utils.py
+++ jupyter_server/utils.py
@@ -10,7 +10,6 @@ import socket
 import sys
 from _frozen_importlib_external import _NamespacePath
 from contextlib import contextmanager
-from distutils.version import LooseVersion
 from urllib.parse import quote
 from urllib.parse import SplitResult
 from urllib.parse import unquote
@@ -20,6 +19,7 @@ from urllib.parse import urlsplit
 from urllib.parse import urlunsplit
 from urllib.request import pathname2url  # pylint: disable=unused-import
 
+from distutils.version import LooseVersion
 from tornado.httpclient import AsyncHTTPClient
 from tornado.httpclient import HTTPClient
 from tornado.httpclient import HTTPRequest
